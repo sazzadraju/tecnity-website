ea9daab3c597daad97051d1f961541b1effdf3ae
Mohammad Sazzad Hossain <70102716+sazzadraju@users.noreply.github.com>
2025-12-09 20:50:28 +0600

    revert: remove serverExternalPackages workaround

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverExternalPackages: ['react', 'react-dom'],
 };
 
 export default nextConfig;
